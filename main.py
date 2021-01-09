23f505390dc8cf31e778f9fc1c678eb97ffd3b42
loxygenk <loxygen.k@gmail.com>
2021-01-09 15:54:06 +0900

    ✨ Generate Discord Client in main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from lib.cmd_dispatch import CommandDispatcher
 from lib.cmds.register import register_commands
 from lib.config.config import Configuration
+from lib.discord.client import DiscordClient
 
 
 def main():
@@ -13,7 +14,8 @@ def main():
         return
 
     config = Configuration()
-    cmd_dispatcher.dispatch_command(sys.argv[1:], config)
+    discord = DiscordClient(config)
+    cmd_dispatcher.dispatch_command(sys.argv[1:], config, discord)
 
 
 if __name__ == "__main__":
