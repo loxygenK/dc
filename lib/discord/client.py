e6794fbc4a915906ebfcdf1eb95de7659f02bad0
loxygenk <loxygen.k@gmail.com>
2021-01-09 15:59:55 +0900

    🐛 Fixed some mistakes

--- lib/discord/client.py
+++ lib/discord/client.py
@@ -1,21 +1,20 @@
+import asyncio
+
 from discord import Client
-from lib.util.colored_print import log, fatal
-import threading
+from lib.util.colored_print import log, fatal, success
+
 
 class DiscordClient(Client):
 
-    def __init__(self, config):
-        super(Client, self).__init__()
+    def __init__(self, config, **options):
+        super().__init__(**options)
         self.token = config.discord_token
         self.on_ready_callback = []
         self.on_message_callback = []
 
-    def __start(self):
-        log("Started discord client. Logging in...")
-        self.run(self.token)
-
-    def start(self):
-        threading.Thread(target=self.__start).start()
+    def access(self):
+        asyncio.ensure_future(self.start(self.token))
+        asyncio.get_event_loop().run_until_complete(self.wait_until_ready())
 
     def add_ready_callback(self, f, *args, **kwargs):
         self.on_ready_callback.append([f, args, kwargs])
@@ -23,13 +22,13 @@ class DiscordClient(Client):
     def add_message_callback(self, f, *args, **kwargs):
         self.on_message_callback.append([f, args, kwargs])
 
-    async def on_ready(self, event):
+    async def on_ready(self):
         for func, args, kwargs in self.on_ready_callback:
             returned_value = func(self, *args, **kwargs)
             if returned_value is not None and not returned_value:
                 fatal("One of callback functions reported error when handling.")
 
-    async def on_mesage(self, event):
+    async def on_message(self, event):
         for func, args, kwargs in self.on_ready_callback:
             returned_value = func(self, event, *args, **kwargs)
             if returned_value is not None and not returned_value:
